662df16651c55f858f5acbba47cdd0debd898cae
Ashley <anm38060@gmail..com>
2025-06-06 10:05:59 -0500

    Initial Commit

---
+++ birthdayAppCamp/birthdayAppCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdayAppCampApp.swift
+//  birthdayAppCamp
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdayAppCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
