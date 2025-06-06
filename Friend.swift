3305eb5494d2fd1fa21a93267d8e2ec0a306cd8c
Ashley <anm38060@gmail..com>
2025-06-06 13:31:13 -0500

    Created UI

---
+++ Friend.swift
@@ -0,0 +1,17 @@
+//
+//  Friend.swift
+//  birthdayAppCamp
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import Foundation
+class Friend {
+    var name: String
+    var birthday: Date
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
+
--- birthdayAppCamp/ContentView.swift
+++ birthdayAppCamp/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [Friend(name: "Ashley", birthday: .now), Friend(name:"Val", birthday: Date(timeIntervalSince1970: 0))]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            List(friends, id: \.name) {friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide) .day() .year())
+                } // end Hstack
+            }//end List
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }//end DatePicker
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    } // end button
+                    .bold()
+                }//end VStack
+                .padding()
+                .background(.bar)
+                
+            }//end safeAreaInset
+        }// end nav stack
+    } //end body
+} // end struct
 
 #Preview {
     ContentView()
